e33d98d171de2f07aca48adfebaf64c610842269
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-08-10 16:01:04 +0530

    updating server.js

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,10 @@ dotenv.config();
 
 const app=express();
 app.use(cors());
-app.use(express.cors());
+app.use(express.json());
+
+const PORT =process.env.PORT || PORT;
+
+app.listen(PORT ,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
