5d0f45533fd505b3e0e7876771a54f77a0ec38f6
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-08-10 21:01:26 +0530

    Server js Updated

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/users',require('./routes/userRoutes'));
+app.use('/api/courses',require('./routes/courseRoutes'));
+app.use('/api/enrollment',require('./routes/enrollmentRoutes'));
+
 const PORT =process.env.PORT || PORT;
 
 app.listen(PORT ,()=>{
