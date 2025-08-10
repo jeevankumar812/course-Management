bcf542e89d5520575c7e7d8a01f26c4d6922b56b
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-08-10 20:53:26 +0530

    update the courseRoute

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,4 +1,10 @@
 const express=require('express');
-const{createCourse, getCourse}=require('../controllers/courseController');
+const{createCourse, getCourses}=require('../controllers/courseController');
+const auth = require('../middlewares/authMiddleware');
 const router=express.Router();
+
+router.post('/',auth,createCourse);
+router.get('/',getCourses)
+
+module.exports=router;
 
