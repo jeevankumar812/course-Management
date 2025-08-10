9910fa44f07d63e262a0ee0a90b8a168970d507f
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-08-10 19:30:52 +0530

    Update on Server.js on about the MongoDB conncetion

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const cors =require('cors');
 const dotenv=require('dotenv');
 const express = require('express');
+const connectDB = require('./config/db');
 
 dotenv.config();
-
+connectDB();
 const app=express();
 app.use(cors());
 app.use(express.json());
