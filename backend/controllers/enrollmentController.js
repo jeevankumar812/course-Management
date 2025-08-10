f5cdf3b5119e90784f9a95b9bd837548283ef41f
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-08-10 20:27:06 +0530

    Created Enroll Course

--- backend/controllers/enrollmentController.js
+++ backend/controllers/enrollmentController.js
@@ -1,3 +1,11 @@
 const Enrollment=require('../models/Enrollment');
 
-const
+exports.enrollCourse=async(req,res)=>{
+    try {
+        const {courseId}=req.body;
+        const enrollment=await Enrollment.create({student :req.user.id, course:courseId});
+        res.status(201).json(enrollment);
+    } catch (error) {
+        res.status(500).json({message:erro.message});
+    }
+};
