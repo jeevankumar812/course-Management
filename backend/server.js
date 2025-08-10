3e5d34ab13835c13f5099934fb696d91999cda87
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-08-10 15:54:43 +0530

    updated Middle ware in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
 const cors =require('cors');
 const dotenv=require('dotenv');
 const express = require('express');
+
+dotenv.config();
+
+const app=express();
+app.use(cors());
+app.use(express.cors());
