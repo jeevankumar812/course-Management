c3b5785ecc5aaedde55451abcfd88e55e3fe9680
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-08-10 19:47:57 +0530

    updated Enrollement js

--- backend/models/Enrollment.js
+++ backend/models/Enrollment.js
@@ -4,4 +4,13 @@ const enrollmentSchema=mongoose.Schema({
     student:{
         type:mongoose.Schema.Types.ObjectId,ref:'User',required:true
     },
-})
+    course:{
+        type:mongoose.Schema.Types.ObjectId,ref:'Course',required:true
+    },
+    enrolledAt:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports=mongoose.model('Enrollment',enrollmentSchema);
